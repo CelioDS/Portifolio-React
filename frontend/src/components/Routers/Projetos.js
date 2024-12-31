ea624c9126ce2ade44e895047362453ce3e5eb27
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2024-12-31 19:03:24 -0300

    FEAT : json serve offline e online

--- frontend/src/components/Routers/Projetos.js
+++ frontend/src/components/Routers/Projetos.js
@@ -15,26 +15,44 @@ export default function Projetos() {
   );
 
   const GetDataBase = async () => {
+    const url = "http://localhost:5000/datsabase";
     try {
-      const url =
-        process.env.REACT_APP_API_URL ||
-        "http://localhost:5000/database" ||
-        "https://raw.githubusercontent.com/CelioDS/Portifolio-React/refs/heads/main/frontend/src/components/backend/db.json";
+      if (url) {
+        const url =
+          process.env.REACT_APP_API_URL ||
+          "http://localhost:5000/database" ||
+          "https://raw.githubusercontent.com/CelioDS/Portifolio-React/refs/heads/main/frontend/src/components/backend/db.json";
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+        const data = await response.json();
+        setTimeout(() => {
+          setLoading(false);
+          toast.success("Dados carregados com sucesso!");
+        }, 100);
+        setDataBase(data);
+      } else {
+        const url =
+          "https://raw.githubusercontent.com/CelioDS/Portifolio-React/refs/heads/main/frontend/src/components/backend/db.json";
 
-      const data = await response.json();
-      setTimeout(() => {
-        setLoading(false);
-        toast.success("Dados carregados com sucesso!");
-      }, 100);
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-      setDataBase(data);
+        const data = await response.Projetos.json();
+        setTimeout(() => {
+          setLoading(false);
+          toast.success("Dados carregados com sucesso!");
+        }, 100);
+        setDataBase(data);
+      }
     } catch (error) {
       toast.error("Erro na base de dados!");
     }
